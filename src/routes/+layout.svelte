4c07e16595067e6f0514b335c9705197e96f9281
Juan Quenga <110294740+JuanQuenga@users.noreply.github.com>
2024-07-23 23:05:15 -0400

    Layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Footer from '$lib/components/Footer.svelte';
+import Header from '$lib/components/Header.svelte';
+  import '../app.css';
+</script>
+
+<div class="leading-normal tracking-normal text-white gradient font-inter">
+  <Header />
+  <slot />
+  <Footer />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import CallToAction from "$lib/components/CallToAction.svelte";
+import Features from "$lib/components/Features.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+import Header from "$lib/components/Header.svelte";
+    import Hero from "$lib/components/Hero.svelte";
+    import LogoClouds from "$lib/components/LogoClouds.svelte";
+    import Pricing from "$lib/components/Pricing.svelte";
+    import ThreeCol from "$lib/components/ThreeCol.svelte";
+</script>
+
+
+<Hero />
+<LogoClouds />
+<Features />
+<ThreeCol />
+<Pricing />
+<CallToAction />  
