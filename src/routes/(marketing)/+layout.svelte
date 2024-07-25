02f73c2eea69afb53125f576a35e1e24378bfc9b
Juan Quenga <110294740+JuanQuenga@users.noreply.github.com>
2024-07-25 16:02:50 -0400

    Update +layout.svelte

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
   import Footer from '$lib/components/Footer.svelte';
   import Header from '$lib/components/Header.svelte';
-  import '../app.css';
+  import '../../app.css';
 </script>
 
 <svelte:head>
