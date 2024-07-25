a2fcc25b25ba0ac4fbf677b55674acbcb6806fd5
Juan Quenga <110294740+JuanQuenga@users.noreply.github.com>
2024-07-25 16:00:16 -0400

    Updated README

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,1 +1,18 @@
-<slot />
+
+<svelte:head>
+  <link
+    rel="preconnect"
+    href="https://cdn.fontshare.com"
+    crossOrigin="anonymous"
+  />
+  <link
+    rel="stylesheet"
+    href="https://api.fontshare.com/v2/css?f[]=satoshi@700,500,400&display=swap"
+  />
+</svelte:head>
+
+<svelte:body class="flex min-h-full" />
+
+<div class="w-full">
+  <slot />
+</div>
--- src/routes/(app)/app/+page.svelte
+++ src/routes/(app)/app/+page.svelte
@@ -1,1 +1,7 @@
-App Here
+<div class="grid grid-cols-1 md:grid-cols-12 w-full bg-yellow-200">
+  <div class="bg-green-200 col-span-12 md:col-span-4 lg:col-span-3 grid grid-cols-6">
+    Green
+  </div>
+  <div class="bg-blue-200 col-span-12 md:col-span-8 lg:col-span-9">Blue</div>
+</div>
+
